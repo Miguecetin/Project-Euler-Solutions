cd66f7ee4ee0ffb5b0a2f26d52f057106ebe0d3c
Miguecetin <miguelang1120@gmail.com>
2025-04-05 00:17:51 +0200

    Problem 5 into 1 line with generator

--- python solutions/5. smallest_multiple.py
+++ python solutions/5. smallest_multiple.py
@@ -7,10 +7,8 @@ def smallest_multiple(rangemax: int = 20) -> int:
     # Date: 04/04/25
     
     # Find the lcm (least common multiple) for numbers [1, n=20] with math.lcm()
-    
-    numbers = [i for i in range(2, rangemax + 1)]
         
-    return lcm(*numbers)
+    return lcm(*[i for i in range(2, rangemax + 1)])
     
 if __name__ == "__main__":
     print(smallest_multiple())
