2214afbec6355ec5b1c84024ff7c01235ce20ae1
Miguecetin <miguelang1120@gmail.com>
2025-04-05 02:05:10 +0200

    Search for prime numbers implemented with math.ceil and math.sqrt

--- python solutions/3. largest_prime_factor.py
+++ python solutions/3. largest_prime_factor.py
@@ -6,11 +6,13 @@
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
 
+import math
+
 def largest_prime_factor(number: int = 600851475143) -> int:
     
     factors = []
     
-    for i in range(1, int(number**0.5 + 1)): # prime numbers must be in range [1, ceil(sqrt(number))]
+    for i in range(1, math.ceil(math.sqrt(number))): # prime numbers must be in range [1, ceil(sqrt(number))]
         
         if number % i == 0: # if number is divisible by i => i is a prime factor
             factors.append(i) # save the prime factor
