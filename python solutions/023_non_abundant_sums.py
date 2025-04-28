06c796dc724d4c33081ab7b0a83c1ec94a9bf8c9
Miguecetin <miguelang1120@gmail.com>
2025-04-28 18:28:43 +0200

    Created file 023

---
+++ python solutions/023_non_abundant_sums.py
@@ -0,0 +1,15 @@
+    #
+    # Solved by Miguecetin
+    # Date: 28/04/25
+    #
+    # https://projecteuler.net/problem=23
+    # https://github.com/Miguecetin/Project-Euler-Solutions
+    #
+
+def non_abundant_sums():
+    pass
+
+if __name__ == "__main__":
+    
+    print(non_abundant_sums())
+    
