33a80b3f8d604e26eab05e8002b76e521543f7af
Miguecetin <miguelang1120@gmail.com>
2025-04-28 18:28:43 +0200

    Problems 21, 22, 23

--- python solutions/021_amicable_numbers.py
+++ python solutions/021_amicable_numbers.py
@@ -5,15 +5,8 @@
     # https://projecteuler.net/problem=21
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
-# TODO test on projecteuler
+
 def amicable_numbers(rangemax: int = 10000) -> int:
-    # TODO remove this explanation
-    # Let d(n) be defined as the sum of proper divisors of n
-    # if d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each a and b are called amicable numbers
-    # Example: d(220) = 1,2,4,5,10,11,20,22,44,55,110 = 284.
-    # Then d(284) = 1,2,4,71,142 = 220
-    # Then, 284 and 220 are amicable numbers
-    # Return the sum of all amicable numbers under 10000
     
     amicables = [] # List of all amicable numbers under 10000
     for a in range(1, rangemax + 1):
@@ -23,7 +16,7 @@ def amicable_numbers(rangemax: int = 10000) -> int:
         divisors_b = get_proper_divisors(b)
         c = d(divisors_b)
         
-        if b == c and b != a:
+        if a == c and b != a:
             amicables.append(a)
             amicables.append(b)
 
@@ -31,7 +24,7 @@ def amicable_numbers(rangemax: int = 10000) -> int:
 
 def get_proper_divisors(number: int) -> list:
     
-    return [i for i in range(1, int(number / 2 + 1)) if i % number == 0]
+    return [i for i in range(1, int(number / 2 + 1)) if number % i == 0]
 
 def d(divisors: list) -> int:
     return sum(i for i in divisors)
