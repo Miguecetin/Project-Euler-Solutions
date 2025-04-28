739d1b64f9ce8fb6fb93b9f0ad5e5dd3c0d4620d
Miguecetin <miguelang1120@gmail.com>
2025-04-28 19:42:19 +0200

    Problem 23

--- python solutions/023_non_abundant_sums.py
+++ python solutions/023_non_abundant_sums.py
@@ -6,10 +6,40 @@
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
 
-def non_abundant_sums():
-    pass
+def non_abundant_sums(rangemax: int = 28123) -> int:
+    # Step 1: Get all abundant numbers in the range [12, 28123]
+    # Step 2: Get all possible sums of two abundant numbers
+    # Step 3: Get all the numbers in the range that aren't stored in the previous step
+    # Step 4: Sum of all numbers in the previous step
+    
+    abundant_numbers = [number for number in range(1, rangemax + 1) if is_abundant(number)]
+    
+    possible_abundant_sums = get_possible_sums(abundant_numbers)
+    
+    non_abundant = [i for i in range(1, rangemax + 1) if i not in possible_abundant_sums]
+    
+    return sum(i for i in non_abundant)
 
+def is_abundant(number: int) -> bool:
+    
+    return number < sum(divisor for divisor in get_proper_divisors(number))
+
+def get_proper_divisors(number: int) -> list:
+    
+    return [i for i in range(1, int(number / 2 + 1)) if number % i == 0]
+
+def get_possible_sums(numbers: list) -> set:
+    
+    sums = set()
+    for number1 in numbers:
+        for number2 in numbers:
+            sums.add(number1 + number2)
+    
+    return sums
+
+    # Alternative via comprehension
+    # return set(number1 + number2 for number1 in numbers for number2 in numbers)
+    
 if __name__ == "__main__":
     
     print(non_abundant_sums())
-    
