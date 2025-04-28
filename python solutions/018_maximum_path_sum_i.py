4e2b01c254d2467e1403a3117cbe3bb06bc7055c
Miguecetin <miguelang1120@gmail.com>
2025-04-28 02:15:52 +0200

    working problem 18

--- python solutions/018_maximum_path_sum_i.py
+++ python solutions/018_maximum_path_sum_i.py
@@ -1,54 +1,48 @@
     #
     # Solved by Miguecetin
-    # Date: 06/04/25
+    # Date: 28/04/25
     #
     # https://projecteuler.net/problem=18
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
     
 def maximum_path_sum(triangle: list) -> int:
-      
-    current_index = 0
-    product_counter = 1
-      
-    for i in range(len(triangle)):
-        
-        tuple = triangle[i]
-        
-        product_counter *= tuple[current_index]
-        
-        if current_index == 0:
-            possible_indexes = (0, 1)
-        else:
-            possible_indexes = (current_index, current_index + 1)
-        
-        if triangle.index(tuple) < len(triangle) - 1:
-            current_index = select_next_index(triangle[triangle.index(tuple) + 1], possible_indexes)
-        
-    return product_counter
-        
-def select_next_index(next_tuple: tuple, possible_indexes: tuple) -> int:
+    current_row = len(triangle) - 2 # Start on the penultimate row and go up (the last row does not have children)
     
-    biggest_possible = max((next_tuple[possible_indexes[0]], next_tuple[possible_indexes[1]]))
+    while current_row >= 0: # While we are not at the top, keep iterating 
     
-    return next_tuple.index(biggest_possible)
+        current_col = 0
+        while current_col < len(triangle[current_row]): # Iterate over every number (column) in the row
         
+            left_child = triangle[current_row + 1][current_col]
+            right_child = triangle[current_row + 1][current_col + 1]
+        
+            triangle[current_row][current_col] += max(left_child, right_child) # Update the number with the sum of itself + the max of its children
+            current_col += 1 # Go to the next column
+        
+        current_row -= 1 # Go to the next row
+
+    # The total max will be in the top
+    return triangle[0][0]
+
 if __name__ == "__main__":
+    
     triangle = [
-        (75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (95, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (17, 47, 82, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (18, 35, 87, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (20, 4, 82, 47, 65, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (19, 1, 23, 75, 3, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-        (88, 2, 77, 73, 7, 63, 67, 0, 0, 0, 0, 0, 0, 0, 0),
-        (99, 65, 4, 28, 6, 16, 70, 92, 0, 0, 0, 0, 0, 0, 0),
-        (41, 41, 26, 56, 83, 40, 80, 70, 33, 0, 0, 0, 0, 0, 0),
-        (41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 0, 0, 0, 0, 0),
-        (53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 0, 0, 0, 0),
-        (70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 0, 0, 0),
-        (91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 0, 0),
-        (63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 0),
-        (4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23)
-        ]
+        [75,],
+        [95, 64],
+        [17, 47, 82],
+        [18, 35, 87, 10],
+        [20, 4, 82, 47, 65],
+        [19, 1, 23, 75, 3, 34],
+        [88, 2, 77, 73, 7, 63, 67],
+        [99, 65, 4, 28, 6, 16, 70, 92],
+        [41, 41, 26, 56, 83, 40, 80, 70, 33],
+        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+        [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+        [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+    ]
+    
     print(maximum_path_sum(triangle))
