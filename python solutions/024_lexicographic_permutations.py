30cfdc1c5e342fa8c606e752b76ef92b104d7350
Miguecetin <miguelang1120@gmail.com>
2025-04-29 14:01:42 +0200

    Problem 24

---
+++ python solutions/024_lexicographic_permutations.py
@@ -0,0 +1,29 @@
+    #
+    # Solved by Miguecetin
+    # Date: 28/04/25
+    #
+    # https://projecteuler.net/problem=24
+    # https://github.com/Miguecetin/Project-Euler-Solutions
+    #
+
+from itertools import permutations
+
+def lexicographic_permutations(digits: tuple = (0,1,2,3,4,5,6,7,8,9), target: int = 1000000) -> int:
+    # Get all lexicographic permutations
+    # Sort them numerically
+    # Get the millionth one
+    
+    perm = permutations(digits)
+    # itertools has a function to calculate the permutations of an iterable
+    
+    perm = sorted(perm)
+    
+    return int("".join(str(i) for i in perm[target - 1]))
+
+    # One-line solution:
+    # return int("".join(str(i) for i in sorted(permutations(digits))[target - 1]))
+    
+    
+if __name__ == "__main__":
+    
+    print(lexicographic_permutations())
