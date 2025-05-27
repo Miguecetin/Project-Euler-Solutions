26d87d959a784978d72b623fbc9701edc55b7439
Miguecetin <miguelang1120@gmail.com>
2025-05-28 01:35:17 +0200

    problem 26 in progress

--- python solutions/026_reciprocal_cycles.py
+++ python solutions/026_reciprocal_cycles.py
@@ -6,25 +6,56 @@
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
     
-def reciprocal_cycles(rangemax: int = 1000) -> int:
+from math import sqrt
     
-    # a = b * q + r //// r = a - (b * q)
+    # The period of the repeating decimal of 1/n is related to the properties of the number n. 
+    # Specifically, it's related to the smallest integer k such that 10^k - 1 is divisible by n.
+    
+    # if get_prime_factors(i) != set([2]) and get_prime_factors(i) != set([5]): # There is no recurring cycle in this case
+    
+def reciprocal_cycles(rangemax: int = 1000) -> int:
     
     fractions_length = dict() # fraction: length
     
-    for fraction in range(1, rangemax):
-        cycle_length = len(str(get_cycle(fraction)))
-        fractions_length[fraction] = cycle_length
+    for fraction in range(2, rangemax):
+        fractions_length[fraction] = get_cycle_length(fraction)
 
-    return max(fractions_length.keys(), key=fractions_length.items()) # max fracción (key) depending on the value
+    return max(fractions_length.keys(), key=fractions_length.get) # max fraction (key) depending on the value
     
-def get_cycle(fraction: int) -> int:
+def get_cycle_length(fraction: int) -> int:
     
-    # a = dividendo = 1
-    # b = divisor = fraction
-    # q = cociente 
+    # Returns the length of the cycle for the given fraction 
     
-    # q = (a - r) / b
+    length = 0
+    
+    if get_prime_factors(fraction) != set([2]) and get_prime_factors(fraction) != set([5]): # There is no recurring cycle in this case
+        pass
+    else: # Cases with recurring cycles
+        
+        aux = True
+        while (aux):
+            length += 1
+            if 10**length % fraction == 0:
+                aux = False
+    
+    return length
+    
+def get_prime_factors(num: int) -> list:
+    
+    prime_factors = set()
+    i = 2
+    
+    while (i ** 2 <= num):
+        if num % i == 0:
+            prime_factors.add(i)
+            num //= i
+        else:
+            i += 1
+            
+    if num > 1:
+        prime_factors.add(num)
+    
+    return prime_factors
     
 if __name__ == "__main__":
     
