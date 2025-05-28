5d38426e0be18a430fdb263c4e5b80baca11cd17
Miguecetin <miguelang1120@gmail.com>
2025-05-28 02:03:11 +0200

    Problem 26

--- python solutions/026_reciprocal_cycles.py
+++ python solutions/026_reciprocal_cycles.py
@@ -6,13 +6,6 @@
     # https://github.com/Miguecetin/Project-Euler-Solutions
     #
     
-from math import sqrt
-    
-    # The period of the repeating decimal of 1/n is related to the properties of the number n. 
-    # Specifically, it's related to the smallest integer k such that 10^k - 1 is divisible by n.
-    
-    # if get_prime_factors(i) != set([2]) and get_prime_factors(i) != set([5]): # There is no recurring cycle in this case
-    
 def reciprocal_cycles(rangemax: int = 1000) -> int:
     
     fractions_length = dict() # fraction: length
@@ -24,38 +17,22 @@ def reciprocal_cycles(rangemax: int = 1000) -> int:
     
 def get_cycle_length(fraction: int) -> int:
     
-    # Returns the length of the cycle for the given fraction 
+    # Remove all factors of 2 and 5 as they do not have cycles
     
+    while fraction % 2 == 0:
+        fraction //= 2
+    while fraction % 5 == 0:
+        fraction //= 5
+
     length = 0
-    
-    if get_prime_factors(fraction) != set([2]) and get_prime_factors(fraction) != set([5]): # There is no recurring cycle in this case
-        pass
-    else: # Cases with recurring cycles
-        
-        aux = True
-        while (aux):
+    if fraction != 1:
+        remainder = 10 % fraction
+        length = 1
+        while remainder != 1:
+            remainder = (remainder * 10) % fraction
             length += 1
-            if 10**length % fraction == 0:
-                aux = False
-    
+
     return length
-    
-def get_prime_factors(num: int) -> list:
-    
-    prime_factors = set()
-    i = 2
-    
-    while (i ** 2 <= num):
-        if num % i == 0:
-            prime_factors.add(i)
-            num //= i
-        else:
-            i += 1
-            
-    if num > 1:
-        prime_factors.add(num)
-    
-    return prime_factors
     
 if __name__ == "__main__":
     
